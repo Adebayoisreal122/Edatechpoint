53b95c608992417b1d5c7cb6012db9cf130a127b
adebayo isreal <adebayoisrealight122@gmail.com>
2024-06-15 18:28:09 +0100

    correcting some errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,29 +22,12 @@ function Navbar() {
     ]
   return (
     <>
-     {/* <nav className="navbar p-1 bg-warning">
-  <div className="container-fluid">
-    <Link to={"/"} className="navbar-brand"><h2>#VANLIFE</h2></Link>
-    <div className="d-flex gap-3" >
-    {
-                navLinks.map((data, i) => {
-                    return (
-                        <div key={i}>
-                            <NavLink className={({ isActive }) => (isActive ? "text-info fw-bolder underline" : "text-dark text-decoration-none")} to={data.path}>{data.name}
-</NavLink>
-                        </div>
-                    );
-                  })
-                }
-                </div>
-               
-    </div>
-</nav>   */}
+    
 
 <nav className="navbar navbar-expand-lg border border-bottom border-body p-1 bg-warning">
   <div className="container-fluid">
     <Link to={"/"} className=" d-flex gap-2 navbar-brand">
-      <img src="./public/images/logo.png"  style={{ height: '40px', width: '40px' }} alt="" />
+      <img src="/images/logo.png"  style={{ height: '40px', width: '40px' }} alt="" />
       <h4>EDATECHPOINT</h4></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
