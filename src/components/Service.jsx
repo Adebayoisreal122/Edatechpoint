b73ef95ce4e62a03d4c872b473d75230415bd11d
adebayo isreal <adebayoisrealight122@gmail.com>
2024-06-17 10:03:06 +0100

    i added some images

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -8,77 +8,77 @@ function Service() {
       <div className="row">
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Residential Real Estate" />
+            <img src="/images/edatech14.webp" className="card-img-top" alt="Residential Real Estate" />
             <div className="card-body">
               <h5 className="card-title">Charing Analysis </h5>
               <p className="card-text">
-                We help you find the perfect home. Our expert agents are here to guide you through every step of the process.
+              Charing analysis involves the detailed examination and mapping of features and boundaries of a piece of land or property. It is a critical step in ensuring accurate representation and understanding of the land's characteristics and constraints.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Commercial Real Estate" />
+            <img src="/images/edatech7.webp" className="card-img-top" alt="Commercial Real Estate" />
             <div className="card-body">
               <h5 className="card-title">Cadastral survey</h5>
               <p className="card-text">
-                Whether you are looking to buy, sell, or lease commercial properties, we have the expertise to assist you.
+              A cadastral survey is the process of creating, marking, defining, and re-establishing property boundaries. It involves recording the dimensions and position of land parcels, often for legal purposes such as land registration, taxation, and property disputes.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Land Surveying" />
+            <img src="/images/edatech23.jpg" className="card-img-top" alt="Land Surveying" />
             <div className="card-body">
               <h5 className="card-title"> Engineering survey </h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              An engineering survey provides the foundational data needed for the planning, design, and construction of infrastructure projects. It includes measuring and mapping the topography, layout, and existing structures within an area to inform engineering designs and plans.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Land Surveying" />
+            <img src="/images/edatech15.webp" className="card-img-top" alt="Land Surveying" />
             <div className="card-body">
               <h5 className="card-title">Real estate consultant </h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              A real estate consultant offers expert advice and services related to property investments, development, management, and transactions. Their role involves market analysis, feasibility studies, property valuation, and strategic planning to help clients make informed decisions.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Land Surveying" />
+            <img src="/images/edatech5.webp" className="card-img-top" alt="Land Surveying" />
             <div className="card-body">
-              <h5 className="card-title"> Giving out a legal document necessary at hand for the right accuracy produced.</h5>
+              <h5 className="card-title"> Legal Document for Accuracy</h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              Issuing a legal document, such as a deed or title, ensures that all necessary legal requirements are met and recorded accurately. These documents are essential for confirming ownership, rights, and boundaries of a property, thereby providing legal protection and accuracy in land transactions and disputes.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Land Surveying" />
+            <img src="/images/bound.jpg" className="card-img-top" alt="Land Surveying" />
             <div className="card-body">
               <h5 className="card-title">Perimeter Survey Plan</h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              A perimeter survey plan outlines the boundaries of a specific parcel of land. It involves measuring and mapping the outer edges of a property to establish clear and precise boundary lines, which are crucial for legal and development purposes.
               </p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src="https://via.placeholder.com/400" className="card-img-top" alt="Land Surveying" />
+            <img src="/images/road.webp" className="card-img-top" alt="Land Surveying" />
             <div className="card-body">
               <h5 className="card-title">Layout design </h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              Layout design refers to the planning and arrangement of physical spaces and structures within a particular area or property. It includes determining the placement of buildings, roads, utilities, and other elements to optimize functionality, aesthetics, and compliance with zoning regulations.
               </p>
             </div>
           </div>
@@ -89,7 +89,7 @@ function Service() {
             <div className="card-body">
               <h5 className="card-title">Modelling design</h5>
               <p className="card-text">
-                Our professional land surveying services ensure accurate and reliable results for your property needs.
+              Modelling design involves creating detailed representations, often in 3D, of structures, landscapes, or systems. These models help visualize and plan projects, allowing for better design accuracy, improved communication among stakeholders, and the identification of potential issues before construction begins.
               </p>
             </div>
           </div>
